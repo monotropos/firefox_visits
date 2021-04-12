e38b87a01777658781bca78193d4f6216f6bf8fd
Apostolos P. Tsompanopoulos <monotropos@gmail.com>
2021-04-12 03:14:16 +0300

    Show last date's visits

--- firefox_visits.py
+++ firefox_visits.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
+import datetime
+# from dateutil import parser
 import sqlite3
 from sqlite3 import Error
-from datetime import datetime
-# from dateutil import parser
 
 
 def open_db(dfile):
@@ -28,14 +28,22 @@ def main(argv):
 	if conn is None:
 		sys.exit(2)
 
-	when = datetime.now().timestamp()
+	when = datetime.datetime.now() - datetime.timedelta(days=1)
+	when = int(when.timestamp() * 1000000)
 	cur = conn.cursor()
-	sql = "SELECT title, url, visit_count FROM moz_places WHERE last_visit_date > " + when
+	sql = "SELECT title, url, visit_count FROM moz_places WHERE last_visit_date > " + str(when) + " ORDER BY last_visit_date"
 	cur.execute(sql)
 	rows = cur.fetchall()
 
 	for row in rows:
-		print(row)
+		if row[0] is None:
+			title = "–"
+		else:
+			title = row[0]
+		try:
+			print(title + "\t" + row[1] + "\t(" + str(row[2]) + ")")
+		except TypeError:
+			print(row)
 
 
 if __name__ == '__main__':
